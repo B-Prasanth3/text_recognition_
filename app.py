28f0db12acfa66dac6f830cffe8cb897bc7a682a
B-Prasanth3 <126316506+B-Prasanth3@users.noreply.github.com>
2024-02-04 18:08:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from PIL import Image
 # Path to your TensorFlow Lite model
 model_path = "model.tflite"
 
+decode_index2numletter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                          'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                          'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 def preprocess_image(image_path, input_size):
     """Preprocess the input image to feed to the TFLite model"""
     img = Image.open(image_path).convert('L')  # Open in grayscale mode
@@ -23,11 +28,6 @@ def set_input_tensor(interpreter, image):
     input_tensor_index = interpreter.get_input_details()[0]['index']
     interpreter.tensor(input_tensor_index)()[0] = image
 
-def get_output_tensor(interpreter, index):
-    """Return the output tensor at the given index."""
-    output = interpreter.get_tensor(interpreter.get_output_details()[index]['index'])
-    return np.squeeze(output)
-
 def recognize_text(image_path, interpreter):
     """Run text recognition on the input image."""
     input_size = (28, 28)  # Replace with the input size expected by your model
@@ -42,10 +42,14 @@ def recognize_text(image_path, interpreter):
     # Get the output tensor
     output_tensor = get_output_tensor(interpreter, 0)
 
-    # Perform post-processing on the output tensor if needed
-    # ...
+    # Find the index of the most probable class
+    index = np.argmax(output_tensor)
 
-    return output_tensor
+    # Map the index to the corresponding character
+    predicted_character = decode_index2numletter[index]
+
+    return predicted_character
+
 
 # Input Fields
 uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "png", "jpeg"])
@@ -61,7 +65,8 @@ if uploaded_file is not None:
     interpreter.allocate_tensors()
 
     # Run text recognition and get the result
-    recognition_result = recognize_text(path, interpreter)
+    predicted_character = recognize_text(path, interpreter)
 
     # Display the recognition result
-    st.write("Recognition Result:", recognition_result)
+    st.write("Predicted Characters:", predicted_character)
+
