0334fb6151ee3d92fefa460782403ede2b47e4b1
B-Prasanth3 <126316506+B-Prasanth3@users.noreply.github.com>
2024-02-15 14:07:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import streamlit as st
+import tensorflow as tf
 from PIL import Image
 import numpy as np
-from keras_ocr.recognition import Recognizer
 
 # Load the TensorFlow Lite model
 model_path = "2.tflite"
 interpreter = tf.lite.Interpreter(model_path=model_path)
 interpreter.allocate_tensors()
-
-# Load the Keras-OCR recognizer
-recognizer = Recognizer(weights="kurapan")
 
 # Get input and output details
 input_details = interpreter.get_input_details()
@@ -32,27 +29,17 @@ def main():
         st.success(f"Text Recognition Result: {result}")
 
 def recognize_text(image):
-    # Get input details
-    input_details = interpreter.get_input_details()
-
-    # Print expected input shape
-    input_shape = input_details[0]['shape']
-    print("Expected Input Shape:", input_shape)
-
     # Preprocess the input image
     input_image = preprocess_image(image)
-
-    # Print actual input shape
-    print("Actual Input Shape:", input_image.shape)
 
     # Run inference
     interpreter.set_tensor(input_details[0]['index'], input_image)
     interpreter.invoke()
 
-    # Get the predicted text using Keras-OCR recognizer
-    result = recognizer.recognize(image)
+    # Get the output
+    output_text = interpreter.get_tensor(output_details[0]['index'])
 
-    return result
+    return output_text
 
 def preprocess_image(image):
     # Get expected dimensions
